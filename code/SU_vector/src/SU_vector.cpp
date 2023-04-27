0c51c65a53f7b2f1e202d54c45f8822d0076add0
Dominik Hellmann <dominik.hellmann@tu-dortmund.de>
2023-04-27 09:55:36 +0200

    added tasks to SU_vector template

--- code/SU_vector/src/SU_vector.cpp
+++ code/SU_vector/src/SU_vector.cpp
@@ -2,6 +2,13 @@
 # include <SQuIDS/SQuIDS.h>
 
 int main(){
+	// task 1: Declare empty SU vector for dim(H) = 3
+
+	// task 2: Initialize array of projectors (B0; mass basis)
+
+	// task 3: Rotate them to (B1; flavor basis)
+
+	// task 4: Declare SU vector corresponding to diag(0, \Delta m_{21}^2, \Delta m_{31}^2)
 	
 	return 0;
 }
