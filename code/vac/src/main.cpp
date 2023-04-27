6c6a468bf2c31055385bdf34eec74cd0f922dca1
Dominik Hellmann <dominik.hellmann@tu-dortmund.de>
2023-04-27 10:06:28 +0200

    added plot file and first tasks

--- code/vac/src/main.cpp
+++ code/vac/src/main.cpp
@@ -4,5 +4,14 @@
 
 int main()
 {
+	// task n: Initialize an object from your class
+
+	// task n + 1: Evolve it for L = 300 km
+	// and for a log scale of energies between (10 MeV to 10 GeV, 100 points)
+
+	// task n + 2: Save the three probability arrays
+	// P_{\nu_e \to \nu_{e} / \nu_{\mu} / \nu_{\tau}}(L, E_j)
+	// to files in data (directory will be automatically generated)
+ 
 	return 0;
 }
---
+++ code/vac/src/plot.py
@@ -0,0 +1,4 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# task n + 3: plot your data in one plot
