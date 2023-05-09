d75f6ab1fb90558d92fb8d762220110ff2a1e363
Dominik Hellmann <dominik.hellmann@tu-dortmund.de>
2023-05-09 19:20:54 +0200

    minor changes

--- code/const/src/const.cpp
+++ code/const/src/const.cpp
@@ -2,21 +2,6 @@
 # include <SQuIDS/SQuIDS.h>
 
 int main(){
-	// initialize SQuIDS onject
-	squids::Const units;
-	
-	// how many inverse electron volts are 300 km?
-	std::cout << "300 km = " << 300 * units.km << " 1 / eV" << "\n";
-	
-	// set mixing angles in rad
-	units.SetMixingAngle(0, 1, 33.48 * units.degree);
-	units.SetMixingAngle(0, 2, 8.55 * units.degree);
-	units.SetMixingAngle(1, 2, 42.3 * units.degree);
-	
-	// set energy differences wrt. ground state
-	units.SetEnergyDifference(1, 7.5e-5 * units.eV * units.eV);
-	units.SetEnergyDifference(2, 2.45e-3 * units.eV * units.eV);
-	
 	// task 1: Default constr. const object
 	squids::Const units;
 
